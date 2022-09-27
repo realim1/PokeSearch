0b8cca8d23c89d18db0643ef5062f3a3aa676153
Markus Realica <markus.realica@creditone.com>
2022-09-27 13:18:19 -0700

    grandmaster: Play around with pokemon data.

--- components/Pokemon/Pokemon.tsx
+++ components/Pokemon/Pokemon.tsx
@@ -5,6 +5,14 @@ const QUERY = gql`
 	query Pokemon_v2_pokemon($where: pokemon_v2_pokemon_bool_exp) {
 		pokemon_v2_pokemon(where: $where) {
 			name
+			pokemon_v2_pokemontypes {
+				pokemon_v2_type {
+					name
+				}
+			}
+			pokemon_v2_pokemonsprites {
+				sprites
+			}
 		}
 	}
 `;
@@ -34,6 +42,23 @@ export default function Pokemon({ identifier }: { identifier: number | null }) {
 		console.error(error);
 		return null;
 	}
+	console.log(data);
+	console.log(
+		JSON.parse(data.pokemon_v2_pokemon[0].pokemon_v2_pokemonsprites[0].sprites)
+	);
 
-	return <div>{data.pokemon_v2_pokemon[0].name}</div>;
+	const sprites = JSON.parse(
+		data.pokemon_v2_pokemon[0].pokemon_v2_pokemonsprites[0].sprites
+	);
+	return (
+		<div>
+			<h1>{data.pokemon_v2_pokemon[0].name}</h1>
+			{data.pokemon_v2_pokemon[0].pokemon_v2_pokemontypes.map(
+				(type: any, key: any) => {
+					return <h2 key={key}>{type.pokemon_v2_type.name}</h2>;
+				}
+			)}
+			<img src={sprites.front_default} alt={data.pokemon_v2_pokemon[0].name} />
+		</div>
+	);
 }
